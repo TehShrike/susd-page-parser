5c5c334793cdb4f1cf86ab57a9bda93236e5364a
Josh Duff <me@JoshDuff.com>
2017-03-28 11:02:16 -0500

    Updated video page fixture
    Includes an empty imageUrl...

--- test-output.js
+++ test-output.js
@@ -1,7 +1,9 @@
 const parse = require('./')
 
-const html = require('fs').readFileSync('./test-fixtures/games-page.html')
+const html = require('fs').readFileSync('./test-fixtures/videos-page.html')
 
 const output = parse(html)
 
-console.log(output)
+console.log(output.filter(item => !item.imageUrl))
+
+// console.log(JSON.stringify(output, null, '\t'))
